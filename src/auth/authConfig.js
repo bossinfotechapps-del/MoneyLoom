11def7188dae73f91a53d81d4d0d4f08198535e6
bossinfotechapps-del <bossinfotech.apps@gmail.com>
2026-09-20 15:01:30 +0530

    chore: set GitHub Pages URLs

--- src/auth/authConfig.js
+++ src/auth/authConfig.js
@@ -7,7 +7,7 @@
 export const WEB_CLIENT_ID = '499260888763-sdhpuf69q65hif62ff6vku721b266qbq.apps.googleusercontent.com';
 
 // Hosted pages linked from the app. See docs/ for the files to publish.
-export const PRIVACY_POLICY_URL = 'https://YOUR-GITHUB-USERNAME.github.io/moneyloom/privacy-policy.html';
-export const TERMS_URL = 'https://YOUR-GITHUB-USERNAME.github.io/moneyloom/terms.html';
-export const DELETE_ACCOUNT_URL = 'https://YOUR-GITHUB-USERNAME.github.io/moneyloom/delete-account.html';
-export const SUPPORT_EMAIL = 'bossinfotech.apps@gmail.com';
+export const PRIVACY_POLICY_URL = 'https://bossinfotechapps-del.github.io/MoneyLoom/privacy-policy.html';
+export const TERMS_URL          = 'https://bossinfotechapps-del.github.io/MoneyLoom/terms.html';
+export const DELETE_ACCOUNT_URL = 'https://bossinfotechapps-del.github.io/MoneyLoom/delete-account.html';
+export const SUPPORT_EMAIL      = 'bossinfotech.apps@gmail.com';
